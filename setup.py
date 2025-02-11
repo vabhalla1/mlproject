7b6eff3514569bb9a9d6c7017dcb7ac1c1178ae7
vabhalla <vabhalla1@gmail.com>
2025-02-11 14:58:08 -0600

    setup and req files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Varun Bhalla',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+)
